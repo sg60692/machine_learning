2201c3836ba46eaa8a9f14625c5b3c845d7dbb66
rivatus <rivatus3@gmail.com>
2021-05-26 15:38:09 +0530

    arpit

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Header from './components/Header/Header.js';
 import Projects from './components/Projects/Projects';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (

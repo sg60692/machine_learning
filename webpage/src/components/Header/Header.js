fb3dbb6f684938c33ce642a73a03eeac06c16b47
Sarthak <61661622+sg60692@users.noreply.github.com>
2021-06-03 18:41:26 +0530

    fixing not responding form

--- webpage/src/components/Header/Header.js
+++ webpage/src/components/Header/Header.js
@@ -4,24 +4,27 @@ import TocIcon from '@material-ui/icons/Toc';
 import CancelIcon from '@material-ui/icons/Cancel';
 function Header() {  
     return (
-        <div id="Header" class="Header-height" >
+        <div>
             <input type="checkbox" id="check"/>
-                <label for="check">
-                    <TocIcon id="nav-btn"/>
-                    <CancelIcon id="nav-cancel"/>
-                </label>
-            <a className="nav-heading">
-                <img src="https://erp.iitbbs.ac.in/lib/img/images/iitb.png"/>
-                <span>
-                    ML
-                </span>
-            </a>
-            <ul className="sidebar">
-                <li><a>Cancer Prediction</a></li>
-                <li><a>Linear Regression</a></li>
-                <li><a>Logistic Regression</a></li>
-                <li><a>Library</a></li>
-            </ul>
+            <label for="check">
+                <TocIcon id="nav-btn"/>
+                <CancelIcon id="nav-cancel"/>
+            </label>
+            <div id="Header" class="H">
+                
+                <a className="nav-heading">
+                    <img src="https://erp.iitbbs.ac.in/lib/img/images/iitb.png"/>
+                    <span>
+                        ML
+                    </span>
+                </a>
+                <ul className="sidebar">
+                    <li><a>Cancer Prediction</a></li>
+                    <li><a>Linear Regression</a></li>
+                    <li><a>Logistic Regression</a></li>
+                    <li><a>Library</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
